a6ded72e94a75b1ea1459940844b5db48ee256f8
saja amer <sajaamer86@gmail.com>
2019-09-29 17:38:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution here
+function findMatching(arr,name){
+  return arr.filter(e=>e.toLowerCase()===name.toLowerCase())
+}
+function fuzzyMatch(arr,name){
+  return arr.filter(e=>e.charAt(0)===name.charAt(0))
+}
